11fba0354f6f5a89e9d7bd57143855e5b5256389
MostakemHossain <mostakememon123@gmail.com>
2023-12-18 00:27:51 +0600

    Validate faculty-department _ ,same course-section

--- src/app/models/offeredCourse/offeredCourse.service.ts
+++ src/app/models/offeredCourse/offeredCourse.service.ts
@@ -1,23 +1,22 @@
-import httpStatus from "http-status";
-import AppError from "../../errors/AppError";
-import { AcademicDepartment } from "../academicDepartment/academicDepartment.model";
-import { AcademicFaculty } from "../academicFaculty/academicFaculty.model";
-import { Course } from "../course/couse.model";
-import { SemesterRegistration } from "../semesterRegistration/semesterRegistration.model";
-import { TOfferedCourse } from "./offeredCourse.interface";
-import { OfferedCourse } from "./offeredCourse.model";
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { AcademicDepartment } from '../academicDepartment/academicDepartment.model';
+import { AcademicFaculty } from '../academicFaculty/academicFaculty.model';
+import { Course } from '../course/couse.model';
+import { SemesterRegistration } from '../semesterRegistration/semesterRegistration.model';
+import { TOfferedCourse } from './offeredCourse.interface';
+import { OfferedCourse } from './offeredCourse.model';
 
-const createOfferedCourseIntoDB= async (payload:TOfferedCourse)=>{
-    const {
-        semesterRegistration,
-        academicFaculty,
-        academicDepartment,
-        course,
-        
-       
-      } = payload;
+const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
+  const {
+    semesterRegistration,
+    academicFaculty,
+    academicDepartment,
+    course,
+    section,
+  } = payload;
 
-       /**
+  /**
    * Step 1: check if the semester registration id is exists!
    * Step 2: check if the academic faculty id is exists!
    * Step 3: check if the academic department id is exists!
@@ -30,51 +29,75 @@ const createOfferedCourseIntoDB= async (payload:TOfferedCourse)=>{
    * Step 10: create the offered course
    */
 
-       const isSemesterRegistrationExits =
-       await SemesterRegistration.findById(semesterRegistration);
-   
-     if (!isSemesterRegistrationExits) {
-       throw new AppError(
-         httpStatus.NOT_FOUND,
-         'Semester registration not found !',
-       );
-     }
-     const academicSemester= isSemesterRegistrationExits?.academicSemester;
-   
-    
-   
-     const isAcademicFacultyExits =
-       await AcademicFaculty.findById(academicFaculty);
-   
-     if (!isAcademicFacultyExits) {
-       throw new AppError(httpStatus.NOT_FOUND, 'Academic Faculty not found !');
-     }
-   
-     const isAcademicDepartmentExits =
-       await AcademicDepartment.findById(academicDepartment);
-   
-     if (!isAcademicDepartmentExits) {
-       throw new AppError(httpStatus.NOT_FOUND, 'Academic Department not found !');
-     }
-   
-     const isCourseExits = await Course.findById(course);
-   
-     if (!isCourseExits) {
-       throw new AppError(httpStatus.NOT_FOUND, 'Course not found !');
-     }
-   
-    //  const isFacultyExits = await faculty.findById(faculty);
-   
-    //  if (!isFacultyExits) {
-    //    throw new AppError(httpStatus.NOT_FOUND, 'Faculty not found !');
-    //  }
-    
+  const isSemesterRegistrationExits =
+    await SemesterRegistration.findById(semesterRegistration);
 
+  if (!isSemesterRegistrationExits) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'Semester registration not found !',
+    );
+  }
+  const academicSemester = isSemesterRegistrationExits?.academicSemester;
 
-    const result= await OfferedCourse.create({...payload,academicSemester});
-    return result;
-}
+  const isAcademicFacultyExits =
+    await AcademicFaculty.findById(academicFaculty);
 
-export const offeredCourseService= {
-   createOfferedCourseIntoDB
-}
+  if (!isAcademicFacultyExits) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Academic Faculty not found !');
+  }
+
+  const isAcademicDepartmentExits =
+    await AcademicDepartment.findById(academicDepartment);
+
+  if (!isAcademicDepartmentExits) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Academic Department not found !');
+  }
+
+  const isCourseExits = await Course.findById(course);
+
+  if (!isCourseExits) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Course not found !');
+  }
+
+  //  const isFacultyExits = await faculty.findById(faculty);
+
+  //  if (!isFacultyExits) {
+  //    throw new AppError(httpStatus.NOT_FOUND, 'Faculty not found !');
+  //  }
+
+  // check if the department is belong to the  faculty
+  const isDepartmentBelongToFaculty = await AcademicDepartment.findOne({
+    _id: academicDepartment,
+    academicFaculty,
+  });
+
+  if (!isDepartmentBelongToFaculty) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `This ${isAcademicDepartmentExits.name} is not  belong to this ${isAcademicFacultyExits.name}`,
+    );
+  }
+  // check if the same offered course same section in same registered semester exists
+
+  const isSameOfferedCourseExistsWithSameRegisteredSemesterWithSameSection =
+    await OfferedCourse.findOne({
+      semesterRegistration,
+      course,
+      section,
+    });
+
+  if (isSameOfferedCourseExistsWithSameRegisteredSemesterWithSameSection) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `Offered course with same section is already exist!`,
+    );
+  }
+
+  const result = await OfferedCourse.create({ ...payload, academicSemester });
+  return result;
+};
+
+export const offeredCourseService = {
+  createOfferedCourseIntoDB,
+};
