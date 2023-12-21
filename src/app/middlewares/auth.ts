3fecec535e154e7a61825b5bd9dead4c90b842ab
MostakemHossain <mostakememon123@gmail.com>
2023-12-21 23:19:19 +0600

    Verify token using auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,16 +1,27 @@
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from '../config';
 import AppError from '../errors/AppError';
 import catchAsync from '../utils/catchAsync';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
+    //check if the token is send from the client
     if(!token){
         throw new AppError(httpStatus.UNAUTHORIZED,'You are not a authorized');
     }
+    // check if the token is valid
+    jwt.verify(token,config.jwt_access_serect as string, function(err,decoded){
+        if(err){
+            throw new AppError(httpStatus.UNAUTHORIZED,'You are not a authorized');
+        }
+       req.user= decoded as JwtPayload
+       next();
+    })
 
-    next();
+
   });
 };
 export default auth;
