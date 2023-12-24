334ede6bd8cfd69ee114aa3da978dd9988a4ead7
MostakemHossain <mostakememon123@gmail.com>
2023-12-24 17:10:47 +0600

    max credits excedded apply condition

--- src/app/models/EnrolledCourse/enrolledCourse.service.ts
+++ src/app/models/EnrolledCourse/enrolledCourse.service.ts
@@ -1,16 +1,18 @@
 import httpStatus from 'http-status';
-import mongoose from 'mongoose';
 import AppError from '../../errors/AppError';
+import { Course } from '../course/couse.model';
 import { OfferedCourse } from '../offeredCourse/offeredCourse.model';
+import { SemesterRegistration } from '../semesterRegistration/semesterRegistration.model';
 import { Student } from '../student/student.model';
 import { TEnrolledCourse } from './enrolledCourse.interface';
 import EnrolledCourse from './enrolledCourse.model';
+import mongoose from 'mongoose';
 
 const createEnrolledCourseIntoDB = async (
   userId: string,
   payload: TEnrolledCourse,
 ) => {
-    console.log(userId,payload);
+  console.log(userId, payload);
   const { offeredCourse } = payload;
   const isOfferedCourseExists = await OfferedCourse.findById(offeredCourse);
   if (!isOfferedCourseExists) {
@@ -21,7 +23,7 @@ const createEnrolledCourseIntoDB = async (
     throw new AppError(httpStatus.BAD_REQUEST, 'Room is Full!');
   }
 
-  const student = await Student.findOne({id:userId});
+  const student = await Student.findOne({ id: userId }, { _id: 1 });
 
   if (!student) {
     throw new AppError(httpStatus.NOT_FOUND, 'Student not Found!');
@@ -32,6 +34,48 @@ const createEnrolledCourseIntoDB = async (
     offeredCourse,
     student: student._id,
   });
+
+  // check max credits exists
+
+  const semesterRegistration = await SemesterRegistration.findOne(
+    isOfferedCourseExists.semesterRegistration,
+  ).select('maxCredit');
+
+  // total enrolled course+ new Enrolled Course Credit> max Credit
+
+  const enrolledCourses = await EnrolledCourse.aggregate([
+    {
+      $match: {
+        semesterRegistration: isOfferedCourseExists.semesterRegistration,
+        student: student._id,
+      },
+    },
+    {
+        $lookup:{
+            from:'courses',
+            localField:'course',
+            foreignField:'_id',
+            as: 'enrollCourseData'
+        }
+    },
+    {
+        $unwind:"$enrollCourseData"
+    },
+    {
+        $group:{_id:null,totalEnrolledCredits:{$sum:'$enrollCourseData.credits'}}
+    },
+    {
+     $project:{_id:0,totalEnrolledCredits:1}
+    }
+  ]);
+  console.log(enrolledCourses);
+  const totalCredits= enrolledCourses.length>0? enrolledCourses[0].totalEnrolledCredits : 0;
+  console.log(totalCredits);
+  const course= await Course.findOne(isOfferedCourseExists.course);
+
+  if(totalCredits && semesterRegistration?.maxCredit && totalCredits+course?.credits > semesterRegistration?.maxCredit){
+    throw new AppError(httpStatus.BAD_REQUEST, 'You have exceeded maximum number of credits');
+  }
 
   if (isStudentAlreadyEnrolled) {
     throw new AppError(httpStatus.CONFLICT, 'Student is Already Enrolled!');
